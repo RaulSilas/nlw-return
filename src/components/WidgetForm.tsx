ef8e81cdc8243e25b32d8ccb5455e563fb763ad3
Raul Silas <raul.oliveira@ntconsult.com.br>
2022-05-03 20:02:03 -0300

    feat: add svg, buttons and style to the form

--- src/components/WidgetForm.tsx
+++ src/components/WidgetForm.tsx
@@ -1,5 +1,33 @@
 import { CloseButton } from "./CloseButton";
 
+import bugImageIrl from "../assets/bug.svg";
+import ideaImageIrl from "../assets/idea.svg";
+import thoughtImageIrl from "../assets/thought.svg";
+
+const feedbackTypes = {
+  BUG: {
+    title: "Problema",
+    image: {
+      source: bugImageIrl,
+      alt: "Imagem de um inseto",
+    },
+  },
+  IDEA: {
+    title: "Idea",
+    image: {
+      source: ideaImageIrl,
+      alt: "Imagem de uma lâmpada",
+    },
+  },
+  OTHER: {
+    title: "Outro",
+    image: {
+      source: thoughtImageIrl,
+      alt: "Imagem de uma nuvem de pensamento",
+    },
+  },
+};
+
 export function WidgetForm() {
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
@@ -8,7 +36,16 @@ export function WidgetForm() {
         <CloseButton />
       </header>
 
-      <div className="flex py-8 gap-2"></div>
+      <div className="flex py-8 gap-2 w-full">
+        {Object.entries(feedbackTypes).map(([key, value]) => {
+          return (
+            <button>
+              <img src={value.image.source} alt={value.image.alt} />
+              <span>{value.title}</span>
+            </button>
+          );
+        })}
+      </div>
 
       <footer className="text-ts text-neutral-400">
         Feito com ♥ pela{" "}
