331cd2846c2747c0ee59e7ccfd2790ce57835cb6
Raul Silas <raul.oliveira@ntconsult.com.br>
2022-05-03 23:49:13 -0300

    feat: success on submit and sucess step

--- src/components/WidgetForm/Index.tsx
+++ src/components/WidgetForm/Index.tsx
@@ -6,6 +6,7 @@ import ideaImageIrl from "../../assets/idea.svg";
 import thoughtImageIrl from "../../assets/thought.svg";
 import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
 import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
+import { FeedbackSuccessStep } from "./Steps/FeedbackSuccessStep";
 
 export const feedbackTypes = {
   BUG: {
@@ -36,20 +37,31 @@ export type FeedbackType = keyof typeof feedbackTypes;
 export function WidgetForm() {
   const [feedbackTypeState, setFeedbackTypeState] =
     useState<FeedbackType | null>(null);
+  const [feedbackSentState, setFeedbackSentState] = useState(false);
 
   function handleRestartFeedback() {
+    setFeedbackSentState(false);
     setFeedbackTypeState(null);
   }
 
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
-      {!feedbackTypeState ? (
-        <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackTypeState} />
-      ) : (
-        <FeedbackContentStep
-          feedbackType={feedbackTypeState}
+      {feedbackSentState ? (
+        <FeedbackSuccessStep
           onFeedbackRestartRequested={handleRestartFeedback}
         />
+      ) : (
+        <>
+          {!feedbackTypeState ? (
+            <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackTypeState} />
+          ) : (
+            <FeedbackContentStep
+              feedbackType={feedbackTypeState}
+              onFeedbackRestartRequested={handleRestartFeedback}
+              onFeedbackSent={() => setFeedbackSentState(true)}
+            />
+          )}
+        </>
       )}
 
       <footer className="text-ts text-neutral-400">
--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,21 +1,31 @@
 import { ArrowLeft } from "phosphor-react";
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 import { CloseButton } from "../../CloseButton";
 import { FeedbackType, feedbackTypes } from "../Index";
 import { ScreenshotButton } from "../ScreenshotButton";
 
 interface FeedbackContentStepProps {
   feedbackType: FeedbackType;
+  onFeedbackSent: () => void;
   onFeedbackRestartRequested: () => void;
 }
 
 export function FeedbackContentStep({
   feedbackType,
   onFeedbackRestartRequested,
+  onFeedbackSent,
 }: FeedbackContentStepProps) {
   const [screenshotState, setScreenshotState] = useState<string | null>(null);
+  const [feedbackCommentState, setFeedbackCommentState] = useState("");
 
   const feedbackTypeInfo = feedbackTypes[feedbackType];
+
+  function handleSubmitFeedback(event: FormEvent) {
+    event.preventDefault();
+    console.log({ screenshotState, feedbackCommentState });
+
+    onFeedbackSent();
+  }
 
   return (
     <>
@@ -41,10 +51,11 @@ export function FeedbackContentStep({
         <CloseButton />
       </header>
 
-      <form className="my-4 w-full">
+      <form onSubmit={handleSubmitFeedback} className="my-4 w-full">
         <textarea
           className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-x-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-none scrollbar-thumb-zinc-500 scrollbar-track-transparent scrollbar-thin"
           placeholder="Conte com detalhes o que está acontecendo..."
+          onChange={(event) => setFeedbackCommentState(event.target.value)}
         />
 
         <footer className="flex gap-2 mt-2">
@@ -55,7 +66,8 @@ export function FeedbackContentStep({
 
           <button
             type="submit"
-            className="p-2 bg-brand-500 rounded-md border-transparent flex flex-1 justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500"
+            className="p-2 bg-brand-500 rounded-md border-transparent flex flex-1 justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 disabled:opacity-50 disabled:hover:bg-brand-500"
+            disabled={feedbackCommentState.length === 0}
           >
             Enviar feedback
           </button>
